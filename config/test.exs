2eb6895f61874b6c4c07ee0188ab3c0e4a8cf449
Luiz Cezer <lccezinha@gmail.com>
2019-04-12 15:02:39 -0300

    Remove config/test.ex and use only config/config.ex

--- config/test.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :goth,
-  json: "#{Path.dirname(__DIR__)}/config/google-pub-sub-fake-data.json" |> File.read!()
-
-config :paddy,
-  project_id: "project_id",
-  topic_id: "topic_id"
